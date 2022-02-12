25d9f6a8e7a97c9d0b6b8138509948fc6bbe332e
Eugene Evseenko <jonikevseenko@gmail.com>
2022-02-13 01:17:33 +0600

    Task 4.3.16

--- Task_4.3.__/Program.cs
+++ Task_4.3.__/Program.cs
@@ -6,26 +6,14 @@ namespace Task_4._3.__
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter чтобы начать");
-            while (Console.ReadLine() != "exit")
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            int counter = 0;
+            foreach (int i in arr)
             {
-                int[] array = new int[20];
-                for (int i = 0; i < 20; i++)
-                {
-                    array[i] = new Random().Next(-100, 100);
-                    Console.Write($"{array[i]} ");
-                }
-                Console.WriteLine();
-                int counter = 0;
-                foreach (int i in array)
-                {
-                    if (i > 0) counter++;
-                }
-                Console.BackgroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine($"В массиве всего {counter} положительных чисел.");
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.WriteLine($"Введите exit чтобы выйти.");
+                if (i > 0) counter++;
             }
+            Console.WriteLine($"В массиве всего {counter} положительных чисел.");
+            Console.ReadKey();
         }
     }
 }
