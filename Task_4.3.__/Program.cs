2c7e4360928d6fdee92dbe032208a86ca1a8bbde
Eugene Evseenko <jonikevseenko@gmail.com>
2022-02-13 01:13:50 +0600

    Task 4.3.15

--- Task_4.3.__/Program.cs
+++ Task_4.3.__/Program.cs
@@ -6,15 +6,26 @@ namespace Task_4._3.__
     {
         static void Main(string[] args)
         {
-            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-            int sum = 0;
-            foreach (int i in arr)
+            Console.WriteLine("Enter чтобы начать");
+            while (Console.ReadLine() != "exit")
             {
-                Console.WriteLine($"{sum} + {i} = {sum + i}");
-                sum += i;
+                int[] array = new int[20];
+                for (int i = 0; i < 20; i++)
+                {
+                    array[i] = new Random().Next(-100, 100);
+                    Console.Write($"{array[i]} ");
+                }
+                Console.WriteLine();
+                int counter = 0;
+                foreach (int i in array)
+                {
+                    if (i > 0) counter++;
+                }
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine($"В массиве всего {counter} положительных чисел.");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine($"Введите exit чтобы выйти.");
             }
-            Console.WriteLine($"Сумма = {sum}");
-            Console.ReadKey();
         }
     }
 }
