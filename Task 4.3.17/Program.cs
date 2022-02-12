96663af5512fc1b78f6624a7886e21bd59a41516
Eugene Evseenko <jonikevseenko@gmail.com>
2022-02-13 01:31:55 +0600

    Task 4.3.17 Init

---
+++ Task 4.3.17/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task_4._3._17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{arr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                int temp;
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    for (int k = j + 1; k < arr.GetLength(1); k++)
+                    {
+                        if ( arr[i, j] > arr[i, k])
+                        {
+                            temp = arr[i, j];
+                            arr[i, j] = arr[i, k];
+                            arr[i, k] = temp;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{arr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
