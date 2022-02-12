d00ee90d31d0b094511a1164b1067b8cbf535f38
Eugene Evseenko <jonikevseenko@gmail.com>
2022-02-13 00:33:10 +0600

    Task 4.3.5

---
+++ Task_4.3.__/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_4._3.__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите своё имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ваше имя по буквам:");
+            foreach (var ch in name)
+            {
+                Console.Write($"{ch} ");
+            }
+            Console.Write($"Последняя буква имени: {name[^1]}");
+            Console.ReadKey();
+        }
+    }
+}
