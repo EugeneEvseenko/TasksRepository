cc39e370f4fc17c7df501b287f1e3bd013e73273
Eugene Evseenko <jonikevseenko@gmail.com>
2022-02-12 22:54:22 +0600

    Task 4.2.11 - do while

--- Task_4.1.17/Program.cs
+++ Task_4.1.17/Program.cs
@@ -6,30 +6,34 @@ namespace Task_4._1._17
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-			switch (Console.ReadLine())
-			{
-				case "red":
-					Console.BackgroundColor = ConsoleColor.Red;
-					Console.ForegroundColor = ConsoleColor.Black;
-					Console.WriteLine("Your color is red!");
-					break;
-				case "green":
-					Console.BackgroundColor = ConsoleColor.Green;
-					Console.ForegroundColor = ConsoleColor.Black;
-					Console.WriteLine("Your color is green!");
-					break;
-				case "cyan":
-					Console.BackgroundColor = ConsoleColor.Cyan;
-					Console.ForegroundColor = ConsoleColor.Black;
-					Console.WriteLine("Your color is cyan!");
-					break;
-				default:
-					Console.BackgroundColor = ConsoleColor.Yellow;
-					Console.ForegroundColor = ConsoleColor.Red;
-					Console.WriteLine("Your color is yellow!");
-					break;
-			}
+			int i = 1;
+			do {
+				Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+				switch (Console.ReadLine())
+				{
+					case "red":
+						Console.BackgroundColor = ConsoleColor.Red;
+						Console.ForegroundColor = ConsoleColor.Black;
+						Console.WriteLine("Your color is red!");
+						break;
+					case "green":
+						Console.BackgroundColor = ConsoleColor.Green;
+						Console.ForegroundColor = ConsoleColor.Black;
+						Console.WriteLine("Your color is green!");
+						break;
+					case "cyan":
+						Console.BackgroundColor = ConsoleColor.Cyan;
+						Console.ForegroundColor = ConsoleColor.Black;
+						Console.WriteLine("Your color is cyan!");
+						break;
+					default:
+						Console.BackgroundColor = ConsoleColor.Yellow;
+						Console.ForegroundColor = ConsoleColor.Red;
+						Console.WriteLine("Your color is yellow!");
+						break;
+				}
+				i++;
+			} while (i <= 3);
 			Console.ReadKey();
 		}
 	}
