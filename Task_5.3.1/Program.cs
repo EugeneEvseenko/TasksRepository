d1c56f40baa19896d686c7ab0821e76110ffaea8
Eugene Evseenko <jonikevseenko@gmail.com>
2022-02-19 22:42:10 +0600

    Task 5.3.1 ref

---
+++ Task_5.3.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_5._3._1
+{
+    internal class Program
+    {
+        static void ChangeAge(ref int age)
+        {
+            Console.WriteLine("Введите возраст");
+            age = int.Parse(Console.ReadLine());
+
+        }
+        static void Main(string[] args)
+        {
+            int age = 25;
+            Console.WriteLine(age);
+            ChangeAge(ref age);
+            Console.WriteLine(age);
+            Console.ReadKey();
+        }
+    }
+}
