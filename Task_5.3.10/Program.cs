9b5f3ce8ac9e6e5c9e15f3794e83486bee2bff70
Eugene Evseenko <jonikevseenko@gmail.com>
2022-02-20 01:18:27 +0600

    Task 5.3.10 - модификатор out

---
+++ Task_5.3.10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_5._3._10
+{
+    internal class Program
+    {
+        /* модификатор out позволяет передавать параметры не объявляя их,
+         * переменные так же передаются в ссылочном виде если были объявлены,
+         * главное отличие от ref, это то что при использовании out
+         * параметр обязан быть изменён.*/
+        static void GetName(out string name, out string oldname)
+        {
+            oldname = "Евгения"; // всегда будет равна этому значению
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine(); // будет равна тому что введут
+        }
+
+        static void Main(string[] args)
+        {
+            /* нет объявления переменных, после 
+             * выполнения функции они будут доступны */
+
+            GetName(out string name, out var oldname);
+
+            Console.WriteLine(name);
+            Console.WriteLine(oldname);
+            Console.ReadKey();
+        }
+    }
+}
