220af54f1b861c79f3a7939ffb575d4de455312b
Eugene Evseenko <jonikevseenko@gmail.com>
2022-02-20 00:58:16 +0600

    Task 5.3.6 - оператор in

---
+++ Task_5.3.6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_5._3._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 2, 3 };
+            int kek = 8; // переменная которую не должна изменить функция
+            BigDataOperation(arr, kek);
+            Console.WriteLine(arr[0]);
+            Console.ReadKey();
+        }
+        /* оператор in позволяет передавать параметры 
+         * в ссылочном виде, при этом запрещает изменять их */
+        static void BigDataOperation(in int[] arr, in int num)
+        {
+            /* изменить можно так как меняется параметр 
+             * не целиком, а изнутри */
+            arr[0] = 4;
+            arr = new int[num]; // такое изменение уже не прокатит
+
+            /* попытка изменения параметра который нельзя
+             * менять внутри функции приводит к ошибке */
+            num = 5; 
+        }
+    }
+}
