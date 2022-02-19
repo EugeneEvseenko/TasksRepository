dd831d288611462d6dd875cf4588b6e1c7d7605a
Eugene Evseenko <jonikevseenko@gmail.com>
2022-02-19 12:06:10 +0600

    Task 4.4.2 Tuple

---
+++ Task_4.4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_4._4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, int age) anketa;
+            Console.Write("Введите имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Введите возраст с цифрами: ");
+            anketa.age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+            
+            Console.ReadKey();
+        }
+    }
+}
