3917f024a24ac09aecc14a71d1b225d707c0391d
Eugene Evseenko <jonikevseenko@gmail.com>
2022-02-20 12:12:31 +0600

    Task 5.5.5 - Factorial

---
+++ Task_5.5.5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_5._5._5
+{
+    internal class Program
+    {
+        static decimal Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Факториал {num} = {Factorial(num)}");
+            Console.ReadKey();
+        }
+    }
+}
