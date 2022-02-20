c04ac56039713d4499db621dfdb3b2d7e2d631a0
Eugene Evseenko <jonikevseenko@gmail.com>
2022-02-20 12:21:35 +0600

    Task 5.5.8 - Pow

---
+++ Task_5.5.8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_5._5._8
+{
+    internal class Program
+    {
+        private static int PowerUp(int N, byte pow)
+        {
+            if(pow == 0)
+            {
+                return 1;
+            }else if(pow == 1)
+            {
+                return N;
+            }
+            else
+            {
+                return N * PowerUp(N, --pow);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите степень");
+            byte pow = byte.Parse(Console.ReadLine());
+            Console.WriteLine($"\n{num} в степени {pow} равна {PowerUp(num, pow)}");
+            Console.ReadKey();
+        }
+    }
+}
