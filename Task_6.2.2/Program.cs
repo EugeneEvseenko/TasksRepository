c0c373d2d3a6e2fb22e021e60e4f80e552be4ee5
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-04 20:53:47 +0600

    Task 6.2.2 - Pen Class

---
+++ Task_6.2.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_6._2._2
+{
+    internal class Program
+    {
+        class Pen
+        {
+            public string color;
+            public int cost;
+            public Pen()
+            {
+                color = "Черный";
+                cost = 100;
+            }
+            public Pen(string penColor, int penCost)
+            {
+                color = penColor;
+                cost = penCost;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Pen p1 = new();
+            Console.WriteLine($"Color = {p1.color} Cost = {p1.cost}");
+            Pen p2 = new("Желтый", 20);
+            Console.WriteLine($"Color = {p2.color} Cost = {p2.cost}");
+            Console.ReadKey();
+        }
+    }
+}
