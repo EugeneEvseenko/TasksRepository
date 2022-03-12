f2ea10c80839729924f812c694dfe8d3d2ac8e15
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-12 17:26:32 +0600

    Task 6.6.1 & 6.6.2 - Class User & TrafficLight

---
+++ Task_6.6.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_6._6._1
+{
+    internal class Program
+    {
+        class TrafficLight
+        {
+            private void ChangeColor(string color)
+            {
+                // принимает цвет
+            }
+            public string GetColor()
+            {
+                return "Состояние светофора.";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_6.6.2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task_6._6._2
+{
+    internal class Program
+    {
+		class User
+		{
+			private int age;
+			private string email;
+			private string login;
+
+			public int Age
+			{
+				get
+				{
+					return age;
+				}
+
+				set
+				{
+					if (value < 18)
+					{
+						Console.WriteLine("Возраст должен быть не меньше 18");
+					}
+					else
+					{
+						age = value;
+					}
+				}
+			}
+			public string Email
+            {
+				get { return email; }
+				set {
+					if (!value.Contains("@"))
+						Console.WriteLine("Поле почты должно содержать знак \"@\".");
+					else
+						email = value;
+				}
+            }
+			public string Login
+            {
+				get { return login; }
+                set
+                {
+					if (value.Length < 3)
+						Console.WriteLine("Поле логина должно быть не менее 3 символов длиной.");
+					else
+						login = value;
+				}
+            }
+		}
+		static void Main(string[] args)
+        {
+			Console.ReadKey();
+        }
+    }
+}
