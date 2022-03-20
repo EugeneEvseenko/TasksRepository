dc014bf7dc2eb02b89ee9f7e8530795f3f99c7b6
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-20 18:57:31 +0600

    Task 7.1.4 - Class Employee

---
+++ Task_7.1.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_7._1._4
+{
+    internal class Program
+    {
+        class Employee
+        {
+            public string Name;
+            public int Age;
+            public int Salary;
+        }
+        class ProjectManager : Employee
+        {
+            public string ProjectName;
+        }
+        class Developer : Employee
+        {
+            public string ProgrammingLanguage;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
