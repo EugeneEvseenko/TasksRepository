e67ddc2ee64ed2011bc8202ec63dc53dc8f4ed75
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-21 15:10:11 +0600

    Task 7.3.3 - Abstract ComputerPart

---
+++ Task_7.3.3/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_7._3._3
+{
+    public abstract class ComputerPart
+    {
+        public abstract void Work();
+    }
+    public class Processor : ComputerPart
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Включаем процессор");
+        }
+    }
+    public class MotherBoard : ComputerPart
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Включаем материнку");
+        }
+    }
+    public class GraphicCard : ComputerPart
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Включаем видеокарту");
+        }
+    }
+}
