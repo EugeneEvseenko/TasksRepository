8252f23ad46ed600c7e921075225eadff57eab68
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-21 12:29:05 +0600

    Task 7.2.4 - Виртуальные свойства и их переопределение

---
+++ Task_7.2.4/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_7._2._4
+{
+    class BaseClass
+    {
+        public virtual int Counter
+        {
+            get;
+            set;
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        private int counter;
+        public override int Counter
+        {
+            get { return counter; }
+            set
+            {
+                if(value >= 0)
+                {
+                    counter = value;
+                }
+            }
+        }
+    }
+}
