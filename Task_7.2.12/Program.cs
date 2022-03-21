08ea2ba70a9020723fca35680bffff55ec175b7f
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-21 13:34:10 +0600

    Task 7.2.12 - Перегрузка операторов

---
+++ Task_7.2.12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task_7._2._12
+{
+    internal class Program
+    {
+        class Obj
+        {
+            public int Value;
+            /*
+             *  Переопределение операции сложения между двумя 
+             *  однотипными объектами типа Obj. Нельзя изменять
+             *  входящие объекты. (можно, но может привести к
+             *  путанице)
+             */
+            public static Obj operator +(Obj a, Obj b)
+            {
+                return new Obj
+                {
+                    Value = a.Value + b.Value
+                };
+            }
+            // Аналогичная операция, но уже вычитания.
+            public static Obj operator -(Obj a, Obj b)
+            {
+                return new Obj
+                {
+                    Value = a.Value - b.Value
+                };
+            }
+        }
+        static void Main(string[] args)
+        {
+            Obj obj1 = new Obj { Value = 1 };
+            Obj obj2 = new Obj { Value = 5 };
+            Console.WriteLine((obj1 + obj2).Value);
+            Console.WriteLine((obj2 - obj1).Value);
+            Console.ReadKey();
+        }
+    }
+}
