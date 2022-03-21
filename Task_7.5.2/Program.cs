1f9b9695fa1b3ddd20043fe254ac475a98ad9759
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-21 22:03:29 +0600

    Task 7.5.2 - Static

---
+++ Task_7.5.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_7._5._2
+{
+    internal class Program
+    {
+        class Obj
+        {
+            public string Name;
+            public string Description;
+            public static int MaxValue = 2000;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"MaxValue = {Obj.MaxValue}");
+            Obj obj1 = new Obj();
+            Obj obj2 = new Obj();
+            Obj.MaxValue = 200;
+            Console.WriteLine($"MaxValue = {Obj.MaxValue}");
+            Console.ReadKey();
+        }
+    }
+}
