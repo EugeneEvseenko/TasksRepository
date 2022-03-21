475ca7015a34a768438f77092c7fa0cd0957abf7
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-21 22:50:15 +0600

    Task 7.6.6 - Multiple <T>

---
+++ Task_7.6.6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_7._6._6
+{
+    internal class Program
+    {
+        class Record<T1, T2>
+        {
+            public T1 Id;
+            public T2 Value;
+            public DateTime date;
+            public override string ToString()
+            {
+                return $"Id: {Id} Value: {Value} Date: {date}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Record<int, int> record = new() { Id = 1, Value = 2, date = DateTime.Now };
+            Record<string, double> record2 = new() { Id = "kek", Value = 2.5, date = DateTime.Now };
+            Console.WriteLine(record.ToString());
+            Console.WriteLine(record2.ToString());
+            Console.ReadKey();
+        }
+    }
+}
