bdc53600174420c42e5fb76cdf155369b42b6c99
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-21 22:37:00 +0600

    Task 7.6.2 - <T>

---
+++ Task_7.6.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_7._6._2
+{
+    internal class Program
+    {
+        class Car<T>
+        {
+            public T Engine;
+            public Car(T engine)
+            {
+                Engine = engine;
+            }
+        }
+        class ElectricEngine { }
+        class GasEngine { }
+        static void Main(string[] args)
+        {
+            var car = new Car<ElectricEngine>(new ElectricEngine());
+            car.Engine = new GasEngine(); // ошибка
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
