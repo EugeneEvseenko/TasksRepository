d28af336116c82823ba85a1ada070222a18fd7ee
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-21 22:10:43 +0600

    Task 7.5.3 - Swap Variables

---
+++ Task 7.5.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_7._5._3
+{
+    internal class Program
+    {
+        public static class Helper
+        {
+            public static void Swap(ref int num1, ref int num2)
+            {
+                int temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int num1 = 3;
+            int num2 = 58;
+
+            Helper.Swap(ref num1, ref num2);
+
+            Console.WriteLine(num1); //58
+            Console.WriteLine(num2); //3
+            Console.ReadKey();
+        }
+    }
+}
