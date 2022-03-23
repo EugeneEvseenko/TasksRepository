5a7eb49f5e993285395f71cd4f03dfb7d2740622
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-23 21:14:44 +0600

    Task 8.2.1 - Подсчет папок и файлов в директории

---
+++ Task_8.2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Task_8._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetCounts(@"C:\");
+            Console.ReadKey();
+        }
+        static void GetCounts(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                string[] dirs = Directory.GetDirectories(path);
+                string[] files = Directory.GetFiles(path);
+                Console.WriteLine($"Количество папок по пути {path}: {dirs.Length}\n" +
+                    $"Количество файлов по пути {path}: {files.Length}");
+            }
+            else
+                Console.WriteLine("Такого пути нет.");
+        }
+    }
+}
