733c88e309b361f3edb13df4f6c06889294b9b72
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-30 21:29:22 +0600

    Final Task 8.2

---
+++ Final_Task_8.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Final_Task_8._2
+{
+    internal class Program
+    {
+        public static long GetDirSize(DirectoryInfo directory, ref long size)
+        {
+            DirectoryInfo[] dirs = directory.GetDirectories();
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                size += file.Length;
+            }
+            foreach (DirectoryInfo dir in dirs)
+            {
+                GetDirSize(dir, ref size);
+            }
+            return size;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь до папки. exit - выход");
+            string text = Console.ReadLine();
+            while (text != "exit")
+            {
+                DirectoryInfo dir = new DirectoryInfo(text);
+                try
+                {
+                    if (dir.Exists)
+                    {
+                        long dirSize = 0;
+                        dirSize = GetDirSize(dir, ref dirSize);
+                        Console.WriteLine(dirSize > 0 ? $"Размер папки: {dirSize} байт" : "Папка пуста.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Папка {text} не существует.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Произошла ошибка доступа к папке {dir.Name}.");
+                    Console.WriteLine(ex.ToString());
+                }
+
+                Console.WriteLine("Введите путь до папки. exit - выход");
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
