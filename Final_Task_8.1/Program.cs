4ca1f39293d2d1ca1d76aed1621eb13d164b455c
Eugene Evseenko <jonikevseenko@gmail.com>
2022-03-30 20:51:19 +0600

    Final Task 8.1 & Task 8.4.1

---
+++ Final_Task_8.1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Final_Task_8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь до папки. exit - выход");
+            string text = Console.ReadLine();
+            while (text != "exit")
+            {
+                DirectoryInfo dir = new DirectoryInfo(text);
+                if (dir.Exists)
+                {
+                    try
+                    {
+                        foreach (DirectoryInfo directory in dir.GetDirectories())
+                        {
+                            if (directory.LastAccessTime <= DateTime.Now.AddMinutes(-30))
+                            {
+                                Console.WriteLine($"Удаляем папку {directory.Name}.");
+                                directory.Delete(true);
+                            }
+                        }
+                        foreach (FileInfo file in dir.GetFiles())
+                        {
+                            if (file.LastAccessTime <= DateTime.Now.AddMinutes(-30))
+                            {
+                                Console.WriteLine($"Удаляем файл {file.Name}.");
+                                file.Delete();
+                            }
+                        }
+                        Console.WriteLine($"Очистка папки {dir.Name} завершена.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Произошла ошибка доступа к папке {dir.Name}.");
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Папка {text} не существует.");
+                }
+                Console.WriteLine("Введите путь до папки. exit - выход");
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Task_8.4.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Task_8._4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // сохраняем путь к файлу (допустим, вы его скачали на рабочий стол)
+            string filePath = $"{AppDomain.CurrentDomain.BaseDirectory}\\BinaryFile.bin";
+
+            // при запуске проверим, что файл существует
+            if (File.Exists(filePath))
+            {
+                // строковая переменная, в которую будем считывать данные
+                string stringValue;
+
+                // считываем, после использования высвобождаем задействованный ресурс BinaryReader
+                using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+                {
+                    stringValue = reader.ReadString();
+                }
+
+                // Вывод
+                Console.WriteLine("Из файла считано:");
+                Console.WriteLine(stringValue);
+            }
+            Console.ReadLine();
+        }
+    }
+}
