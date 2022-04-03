1af538fe0bc7595b380618105626ee9e8bd69660
Eugene Evseenko <jonikevseenko@gmail.com>
2022-04-03 15:09:53 +0600

    Task 9.3.12 Анонимные методы

---
+++ Task_9.3.12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_9._3._12
+{
+    internal class Program
+    {
+        delegate void ShowMessageDelegate(string _message);
+        static void Main(string[] args)
+        {
+            ShowMessageDelegate sd = delegate (string _message)
+            {
+                Console.WriteLine(_message);
+            };
+            sd.Invoke("Hello world!");
+            Console.Read();
+        }
+    }
+}
---
+++ Task_9.3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_9._3._2
+{
+    internal class Program
+    {
+        delegate int CalculateDelegate(int a, int b);
+        static void Main(string[] args)
+        {
+
+            CalculateDelegate calcDelegate = Calculate;
+            int result = calcDelegate(100, 30);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+
+        static int Calculate(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
