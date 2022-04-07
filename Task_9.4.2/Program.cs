db0b0141aae0b72ddc1bee5f7af1bcbc9059927d
Eugene Evseenko <jonikevseenko@gmail.com>
2022-04-07 20:39:38 +0600

    Task 9.4.2 - Ковариантность

---
+++ Task_9.4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_9._4._2
+{
+    internal class Program
+    {
+        public class Car { }
+        public class Lexus : Car { }
+        public delegate Car HandlerMethod();
+
+        public static Car CarHandler() => null;
+
+        public static Lexus LexusHandler() => null;
+        static void Main(string[] args)
+        {
+            HandlerMethod handlerLexus = LexusHandler; // ковариантость
+
+            Console.Read();
+        }
+    }
+}
