c834a8ae655eab156f1a1ef8caa9c060585d51f1
Eugene Evseenko <jonikevseenko@gmail.com>
2022-04-09 13:02:58 +0600

    Task 10.4.4 - Контрвариантность интерфейсов

---
+++ Task_10.4.4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_10._4._4
+{
+    public class User { }
+
+    public class Account : User { }
+    public interface IUpdater<in T>
+    {
+        void Update(T entity);
+    }
+    public class UserService : IUpdater<User>
+    {
+        public void Update(User entity)
+        {
+            Console.WriteLine(entity);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+            var account = new Account();
+
+            IUpdater<Account> updater = new UserService(); // контрвариация
+
+            var userService = new UserService();
+            userService.Update(user);
+            Console.ReadKey();
+        }
+    }
+}
