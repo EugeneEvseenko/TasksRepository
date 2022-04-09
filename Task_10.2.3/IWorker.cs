5019fd4815d86a7f581d8e7cf2d5c109095230ed
Eugene Evseenko <jonikevseenko@gmail.com>
2022-04-09 11:39:21 +0600

    Task 10.2.3 - Неявная реализация интерфейса

---
+++ Task_10.2.3/IWorker.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Task_10._2._3
+{
+    public interface IWorker
+    {
+        public void Build();
+    }
+}
---
+++ Task_10.2.3/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10._2._3
+{
+    public class Worker : IWorker
+    {
+        public void Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
