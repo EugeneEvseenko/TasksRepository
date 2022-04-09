8e8c6a2561436bf6b8c83172cd77cf281387591d
Eugene Evseenko <jonikevseenko@gmail.com>
2022-04-09 12:10:05 +0600

    Task 10.3.3 - Множественная явная реализация интерфейса

---
+++ Task_10.3.3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task_10._3._3
+{
+    internal class Program
+    {
+        public interface IBook
+        {
+            void Read();
+        }
+
+        public interface IDevice
+        {
+            void TurnOn();
+            void TurnOff();
+        }
+        public class ElectronicBook : IBook, IDevice
+        {
+            void IBook.Read()
+            {
+                Console.WriteLine("Метод Read");
+            }
+
+            void IDevice.TurnOff()
+            {
+                Console.WriteLine("Метод TurnOff");
+            }
+
+            void IDevice.TurnOn()
+            {
+                Console.WriteLine("Метод TurnOn");
+            }
+        }
+        static void Main(string[] args)
+        {
+            IBook book = new ElectronicBook();
+            book.Read(); // доступ только к методу для чтения
+
+            ElectronicBook electronicBook = new(); // нет доступа ни к одному методу
+            ((IDevice)electronicBook).TurnOff(); // доступ появляется только при явном обращении
+            ((IDevice)electronicBook).TurnOn(); // и тут
+            ((IBook)electronicBook).Read(); // и тут
+            Console.ReadKey();
+        }
+    }
+}
