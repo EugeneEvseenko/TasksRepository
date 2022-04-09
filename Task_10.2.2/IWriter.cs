358576fcfae87f7475af50adb305289aaab0dec0
Eugene Evseenko <jonikevseenko@gmail.com>
2022-04-09 11:37:10 +0600

    Task 10.2.2 - Явная реализация интерфейса

---
+++ Task_10.2.2/IWriter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Task_10._2._2
+{
+    public interface IWriter
+    {
+        void Write();
+    }
+}
---
+++ Task_10.2.2/Writer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10._2._2
+{
+    public class Writer : IWriter
+    {
+        void IWriter.Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
