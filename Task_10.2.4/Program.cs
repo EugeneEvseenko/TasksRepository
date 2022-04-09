efa95f0263ff844ff1c8b411e042ddd2d469a6a4
Eugene Evseenko <jonikevseenko@gmail.com>
2022-04-09 11:43:20 +0600

    Task 10.2.4 - Явная реализация интерфейса и вызов метода

---
+++ Task_10.2.4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_10._2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            ((IWorker)worker).Build(); // явный вызов, worker.Build(); не сработает.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_10.2.4/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10._2._4
+{
+    public class Worker : IWorker
+    {
+        void IWorker.Build()
+        {
+            Console.WriteLine("Вызван метод Build()");
+        }
+    }
+}
