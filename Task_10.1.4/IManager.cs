628d65859d9c094b9fd03ba52ed1336b88bc2bd7
Eugene Evseenko <jonikevseenko@gmail.com>
2022-04-09 11:26:36 +0600

    Task 10.1.4 - Interface

---
+++ Task_10.1.4/IManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10._1._4
+{
+    public interface IManager
+    {
+        public void Create();
+        public void Read();
+        public void Update();
+        public void Delete();
+
+    }
+}
---
+++ Task_10.1.4/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_10._1._4
+{
+    public class Manager : IManager
+    {
+        public void Create() { }
+
+        public void Read() { }
+
+        public void Update() { }
+
+        public void Delete() { }
+    }
+}
