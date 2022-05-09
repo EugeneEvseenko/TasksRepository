7ea3f7fc2f2e1f0d724aecc1f0e2c0bdd7aeb9eb
Eugene Evseenko <jonikevseenko@gmail.com>
2022-05-09 16:36:12 +0600

    Task 14.1.6 - LINQ - Merge arrays in list and sort all

---
+++ Task_14.1.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_14._1._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numsList = new List<int[]>()
+            {
+               new[] {2, 3, 7, 1},
+               new[] {45, 17, 88, 0},
+               new[] {23, 32, 44, -6},
+            };
+
+            var orderedNums = numsList
+                .SelectMany(s => s)
+                .OrderBy(s => s);
+
+            foreach (var num in orderedNums)
+                Console.WriteLine(num);
+
+            Console.ReadKey();
+        }
+    }
+}
