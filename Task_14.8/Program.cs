16d3bbdc1bc10205b6e08ce85adacee7abc4810c
Eugene Evseenko <jonikevseenko@gmail.com>
2022-05-09 16:23:11 +0600

    Task 14.8 - Select from list and sort result by linq

---
+++ Task_14.8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_14._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var objects = new List<object>()
+            {
+               1,
+               "Сергей ",
+               "Андрей ",
+               300,
+            };
+
+            var names = objects.Where(x => x is string).OrderBy(n => n);
+            foreach (var name in names)
+                Console.WriteLine(name);
+            Console.ReadKey();
+        }
+    }
+}
