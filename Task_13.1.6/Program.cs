7527d4961d717c882ff039ae178610c2a1e99537
Eugene Evseenko <jonikevseenko@gmail.com>
2022-05-09 11:12:57 +0600

    Task 13.1.6 & 13.3.4

---
+++ Task_13.1.6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Task_13._1._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}\\cdev_Text.txt");
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(words.Length);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_13.3.4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_13._3._4
+{
+    internal class Program
+    {
+        public class Contact
+        {
+            public Contact(string name, long phoneNumber, String email)
+            {
+                Name = name;
+                PhoneNumber = phoneNumber;
+                Email = email;
+            }
+
+            public String Name { get; }
+            public long PhoneNumber { get; }
+            public String Email { get; }
+        }
+        
+        public static void AddUnique(Contact contact, List<Contact> phoneBook)
+        {
+            bool isExist = false;
+            foreach (var item in phoneBook)
+            {
+                if (item.Name == contact.Name)
+                {
+                    isExist = true; break;
+                }
+            }
+            if (!isExist)
+            {
+                phoneBook.Add(contact);
+                Console.WriteLine($"Контакт \"{contact.Name}\" добавлен!");
+            }
+            else
+                Console.WriteLine($"Контакт \"{contact.Name}\" уже существует!");
+
+            Console.WriteLine("Список контактов:");
+            foreach (var item in phoneBook)
+                Console.WriteLine($"{item.Name} {item.PhoneNumber} {item.Email}");
+            Console.WriteLine("\n");
+        }
+        static void Main(string[] args)
+        {
+            List<Contact> contacts = new();
+            AddUnique(new Contact("Иван", 77081456821, "ivan@email.com"), contacts);
+            AddUnique(new Contact("Иван", 77081456821, "ivan@email.com"), contacts);
+            AddUnique(new Contact("Олег", 77471478523, "oleg@email.com"), contacts);
+            AddUnique(new Contact("Женя", 77071759988, "john@email.com"), contacts);
+            AddUnique(new Contact("Саша", 7702321854, "sasha@email.com"), contacts);
+            AddUnique(new Contact("Олег", 77471478523, "oleg@email.com"), contacts);
+            Console.ReadKey();
+        }
+    }
+}
