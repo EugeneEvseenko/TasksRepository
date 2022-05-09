fdb5c2b45498896ebd53c11b7932e604728d9d40
Eugene Evseenko <jonikevseenko@gmail.com>
2022-05-09 16:45:11 +0600

    Task 14.2.1 - LINQ - Select to anonymous type and sort

---
+++ Task_14.2.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_14._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха" };
+
+            var animals = words
+                .Select(x => new { Name = x, NameLenght = x.Length})
+                .OrderBy(x => x.NameLenght);
+
+            foreach(var animal in animals)
+                Console.WriteLine(animal);
+
+            Console.ReadKey();
+        }
+    }
+}
