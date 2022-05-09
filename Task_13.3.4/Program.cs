be3052b71d50fd87e4ce9d7601607da3d34e2393
Eugene Evseenko <jonikevseenko@gmail.com>
2022-05-09 12:25:58 +0600

    Change list to dictionary

--- Task_13.3.4/Program.cs
+++ Task_13.3.4/Program.cs
@@ -7,50 +7,40 @@ namespace Task_13._3._4
     {
         public class Contact
         {
-            public Contact(string name, long phoneNumber, String email)
+            public Contact(long phoneNumber, String email)
             {
-                Name = name;
                 PhoneNumber = phoneNumber;
                 Email = email;
             }
 
-            public String Name { get; }
             public long PhoneNumber { get; }
             public String Email { get; }
         }
         
-        public static void AddUnique(Contact contact, List<Contact> phoneBook)
+        public static void AddUnique(string name, Contact contact, Dictionary<string, Contact> phoneBook)
         {
-            bool isExist = false;
-            foreach (var item in phoneBook)
+            if (!phoneBook.ContainsKey(name))
             {
-                if (item.Name == contact.Name)
-                {
-                    isExist = true; break;
-                }
-            }
-            if (!isExist)
-            {
-                phoneBook.Add(contact);
-                Console.WriteLine($"Контакт \"{contact.Name}\" добавлен!");
+                phoneBook.Add(name, contact);
+                Console.WriteLine($"Контакт \"{name}\" добавлен!");
             }
             else
-                Console.WriteLine($"Контакт \"{contact.Name}\" уже существует!");
+                Console.WriteLine($"Контакт \"{name}\" уже существует!");
 
             Console.WriteLine("Список контактов:");
             foreach (var item in phoneBook)
-                Console.WriteLine($"{item.Name} {item.PhoneNumber} {item.Email}");
+                Console.WriteLine($"{item.Key} {item.Value.PhoneNumber} {item.Value.Email}");
             Console.WriteLine("\n");
         }
         static void Main(string[] args)
         {
-            List<Contact> contacts = new();
-            AddUnique(new Contact("Иван", 77081456821, "ivan@email.com"), contacts);
-            AddUnique(new Contact("Иван", 77081456821, "ivan@email.com"), contacts);
-            AddUnique(new Contact("Олег", 77471478523, "oleg@email.com"), contacts);
-            AddUnique(new Contact("Женя", 77071759988, "john@email.com"), contacts);
-            AddUnique(new Contact("Саша", 7702321854, "sasha@email.com"), contacts);
-            AddUnique(new Contact("Олег", 77471478523, "oleg@email.com"), contacts);
+            Dictionary<string, Contact> contacts = new();
+            AddUnique("Иван", new Contact(77081456821, "ivan@email.com"), contacts);
+            AddUnique("Иван", new Contact(77081456821, "ivan@email.com"), contacts);
+            AddUnique("Олег", new Contact(77471478523, "oleg@email.com"), contacts);
+            AddUnique("Женя", new Contact(77071759988, "john@email.com"), contacts);
+            AddUnique("Саша", new Contact(7702321854, "sasha@email.com"), contacts);
+            AddUnique("Олег", new Contact(77471478523, "oleg@email.com"), contacts);
             Console.ReadKey();
         }
     }
