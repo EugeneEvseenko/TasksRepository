1366a10a117c2b625c97b962a80f81e29b5474b2
Eugene Evseenko <jonikevseenko@gmail.com>
2022-05-21 11:05:12 +0600

    Task 15.1.4 | Intersect | LINQ

---
+++ Task_15.1.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_15._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое слово");
+            string first = Console.ReadLine();
+
+            Console.WriteLine("Введите второе слово");
+            string second = Console.ReadLine();
+
+            var kek = first.Intersect(second);
+            Console.WriteLine("Пересечиния:");
+            if(kek.Count() > 0)
+                foreach (var item in kek)
+                    Console.Write($"{item} ");
+            else
+                Console.WriteLine($"В словах '{first}' и '{second}' пересечений нет.");
+
+            Console.ReadKey();
+        }
+    }
+}
