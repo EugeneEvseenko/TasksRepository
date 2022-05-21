4a9c9adca064c25fe97d45332e82b2e6803f71c4
Eugene Evseenko <jonikevseenko@gmail.com>
2022-05-21 11:26:59 +0600

    Task 15.1.6 | Distinct | LINQ

---
+++ Task_15.1.6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_15._1._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string line = Console.ReadLine();
+            var exceptSymbols = new char[] { ',', '.', ';', ':',  '?', '!' };
+
+            //var uniqueChars = line.Except(exceptSymbols).Distinct();
+            var uniqueChars = line.Where(x => !char.IsPunctuation(x)).Distinct();
+
+            foreach (var ch in uniqueChars)
+                Console.Write($" {ch}");
+
+            Console.ReadKey();
+        }
+    }
+}
