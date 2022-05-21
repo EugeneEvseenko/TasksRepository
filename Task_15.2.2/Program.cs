4236be7a5c0e187509e6afa1328520b734312fe9
Eugene Evseenko <jonikevseenko@gmail.com>
2022-05-21 11:49:55 +0600

    Task 15.2.8 | Sum, Max, Min, Average | LINQ

---
+++ Task_15.2.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_15._2._2
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public long Phone { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var contacts = new List<Contact>()
+            {
+               new Contact() { Name = "Андрей", Phone = 79994500508 },
+               new Contact() { Name = "Сергей", Phone = 799990455 },
+               new Contact() { Name = "Иван", Phone = 79999675334 },
+               new Contact() { Name = "Игорь", Phone = 8884994 },
+               new Contact() { Name = "Анна", Phone = 665565656 },
+               new Contact() { Name = "Василий", Phone = 3434 }
+            };
+
+            var countFailed =
+                contacts.Count(x =>
+                       x.Phone.ToString().Length != 11
+                    || !x.Phone.ToString().StartsWith("7"));
+
+
+            Console.WriteLine($"Количество неправильных номеров: {countFailed}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_15.2.8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task_15._2._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new();
+            while (true)
+            {
+                
+                enternumber: Console.WriteLine("Введите новое число:");
+                var inputLine = Console.ReadLine();
+                Console.Clear();
+                if (int.TryParse(inputLine, out int number))
+                    numbers.Add(number);
+                else
+                {
+                    Console.WriteLine("Вводить нужно число."); goto enternumber;
+                }
+
+                Console.WriteLine($"Сейчас в списке чисел: {numbers.Count}\n" +
+                    $"Сумма чисел: {numbers.Sum()}\n" +
+                    $"Наибольшее из них: {numbers.Max()}\n" +
+                    $"Наименьшее: {numbers.Min()}\n" +
+                    $"Среднее ариметическое: {numbers.Average()}\n");
+            }
+            
+        }
+    }
+}
